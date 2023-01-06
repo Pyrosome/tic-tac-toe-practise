99b92d58ec5811b008286fe8d702dcf7728c60ca
Gaultheria <valeriaburina@gmail.com>
2023-01-06 02:30:35 +0100

    tic tac toe brocken reset

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-const content = document.querySelector(".content");
-
-// adding markup in js
-for (let i = 0; i < 9; i++) {
-  content.insertAdjacentHTML(
-    "beforeend",
-    `<div class="item" data-id="${1 + i}"></div>`
-  );
-}
-
-let player = "X";
+const content = document.querySelector('.content');
+let markup = '';
+let player = 'X';
 let stepX = [];
 let stepO = [];
+const win = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-content.addEventListener("click", onClick);
+for (let i = 0; i < 9; i++) {
+  markup += `<div class="item" data-id="${1 + i}"></div>`;
+}
+content.insertAdjacentHTML('beforeend', markup); // adding markup in js
+
+// ---- make a move ---- //
+content.addEventListener('click', onClick);
 function onClick(event) {
   if (!event.target.textContent) {
-    const id = event.target.dataset.id;
-    event.target.textContent = player; // write a symbol of the player on the feild
-    player === "X" ? stepX.push(id) : stepO.push(id); // get div.item id for the player
-    player = player === "X" ? "O" : "X"; // change to next player
+    const id = Number(event.target.dataset.id);
+    if (player === 'X') {
+      stepX.push(id); // += div.item number the player choose
+      const isWinner = checkWin(stepX);
+      if (isWinner) {
+        alert(`We have a winner! It's ${player}`);
+        doReset();
+        return;
+      }
+    } else {
+      stepO.push(id);
+      const isWinner = checkWin(stepO);
+      if (isWinner) {
+        alert(`We have a winner! It's ${player}`);
+        doReset();
+        return;
+      }
+    }
+
+    event.target.textContent = player; // write a symbol of the player
+    player = player === 'X' ? 'O' : 'X'; // change to next player
   }
 }
 
+// ---- check winner ---- //
+function checkWin(arr) {
+  const result = win.some((values) => values.every((value) => arr.includes(value)));
+  return result;
+}
+
+// ---- reset feild ---- //
 const reset = document.querySelector("[type='button']"); // reset button
-const items = Array.from(document.querySelectorAll(".item")); // div.items nodelist to array
-// reset by click button
-reset.addEventListener("click", () => {
-  items.forEach((item) => (item.textContent = ""));
-  console.log(stepX);
+reset.addEventListener('click', doReset);
+
+function doReset() {
+  markup = '';
   stepX = [];
   stepO = [];
-});
+  player = 'X';
+  console.log(player);
+}
+
+// const items = Array.from(document.querySelectorAll(".item")); // div.items nodelist to array
+// reset.addEventListener("click", () => {
+//   items.forEach((item) => (item.textContent = ""));
+// });
